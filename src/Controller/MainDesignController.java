c8b793245b19fd1b9891509a4cf3defb545e46c7
Emran Hosen Emon <emranhosen.emon11@gmail.com>
2021-07-13 16:52:02 +0600

    Complete DOC Pane Design
    Total DISPLAY OF CALCULATOR Pane Complete and Add all Object in Control Class.

--- src/Controller/MainDesignController.java
+++ src/Controller/MainDesignController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
 import java.net.URL;
@@ -16,6 +17,12 @@ public class MainDesignController implements Initializable
 
     @FXML
     Button CalculatorButton,ConverterButton,MortgageButton;
+
+    @FXML
+    Button one,two,three,four,five,six,seven,eight,nine,zero,zero_zero,full_stop,multi,plus,subtraction,equal,slash,percent,x,ac;
+
+    @FXML
+    Label userValue,output;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
