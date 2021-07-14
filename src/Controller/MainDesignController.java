fc6e6804177a1bd5315454089718b14e593f3cee
Emran Hosen Emon <emranhosen.emon11@gmail.com>
2021-07-14 20:08:11 +0600

    Calculation Work Perfect
    ADD, Subtraction, Multiple, Division, Percent .Work Perfect but on bye one button have Problem. I will try to fix now this file I will Commit to Main.

--- src/Controller/MainDesignController.java
+++ src/Controller/MainDesignController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -29,11 +30,17 @@ public class MainDesignController implements Initializable
     int ValueOne=1,ValueTwo=2,ValueThree=3,ValueFour=4,ValueFive=5,ValueSix=6,ValueSeven=7,ValueEight=8,ValueNine=9,ValueZero=0;
     String ValueZeroZero="00",ValueFull_Stop=".",ValueMulti="x",ValuePlus="+",ValueSubtraction="-",ValueEqual="=",ValueSlash="/",ValuePercent="%",ValueX="x",ValueAC= "";
     char lastValue;
+    double StoreCalculationValue=0,BeforeCalValue,AfterCalValue;
+    public String SplitString="";
+    public String PublicOperator="NULL";
+    public int conditionForOperator;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        x.setDisable(true);
+        fontSizeRestore();
     }
+
 
 
     public void ActionEvent(ActionEvent actionEvent)
@@ -62,60 +69,91 @@ public class MainDesignController implements Initializable
     {
         if(actionEventForCB.getSource()==one)
         {
+            fontSizeRestore();
             PrintMethod(ValueOne,"Emran");
+            OperandStore(Integer.toString(ValueOne));
+            return;
         }
         else if(actionEventForCB.getSource()==two)
         {
+            fontSizeRestore();
             PrintMethod(ValueTwo,"Emran");
+            OperandStore(Integer.toString(ValueTwo));
+            return;
         }
         else if(actionEventForCB.getSource()==three)
         {
+            fontSizeRestore();
             PrintMethod(ValueThree,"Emran");
+            OperandStore(Integer.toString(ValueThree));
+            return;
         }
         else if(actionEventForCB.getSource()==four)
         {
+            fontSizeRestore();
             PrintMethod(ValueFour,"Emran");
+            OperandStore(Integer.toString(ValueFour));
+            return;
         }
         else if(actionEventForCB.getSource()==five)
         {
+            fontSizeRestore();
             PrintMethod(ValueFive,"Emran");
+            OperandStore(Integer.toString(ValueFive));
+            return;
         }
         else if(actionEventForCB.getSource()==six)
         {
+            fontSizeRestore();
             PrintMethod(ValueSix,"Emran");
+            OperandStore(Integer.toString(ValueSix));
+            return;
         }
         else if(actionEventForCB.getSource()==seven)
         {
+            fontSizeRestore();
             PrintMethod(ValueSeven,"Emran");
+            OperandStore(Integer.toString(ValueSeven));
+            return;
         }
         else if(actionEventForCB.getSource()==eight)
         {
+            fontSizeRestore();
             PrintMethod(ValueEight,"Emran");
+            OperandStore(Integer.toString(ValueEight));
+            return;
         }
         else if(actionEventForCB.getSource()==nine)
         {
+            fontSizeRestore();
             PrintMethod(ValueNine,"Emran");
+            OperandStore(Integer.toString(ValueNine));
+            return;
         }
         else if(actionEventForCB.getSource()==zero)
         {
+            fontSizeRestore();
             PrintMethod(ValueZero,"Emran");
+            OperandStore(Integer.toString(ValueZero));
+            return;
         }
         else if(actionEventForCB.getSource()==zero_zero)
         {
+            fontSizeRestore();
             PrintMethod(-1,ValueZeroZero);
+            OperandStore(ValueZeroZero);
+            return;
         }
 
-        else if(actionEventForCB.getSource()==full_stop)
-        {
-            PrintMethod(-1,ValueFull_Stop);
-        }
-        else if(actionEventForCB.getSource()==percent)
-        {
-            PrintMethod(-1,ValuePercent);
-        }
         else if(actionEventForCB.getSource()==equal)
         {
-            PrintMethod(-1,ValueEqual);
+            //calculationMethod();
+
+            userValue.setFont(new Font("Serif",13));
+            output.setFont(new Font("Serif", 25));
+            userValue.setText(userValue.getText());
+            output.setText(String.valueOf(StoreCalculationValue));
+            return;
         }
         else if(actionEventForCB.getSource()==x)
         {
@@ -133,11 +171,17 @@ public class MainDesignController implements Initializable
         }
         else if(actionEventForCB.getSource()==ac)
         {
+            fontSizeRestore();
+            StoreCalculationValue=0;
+            BeforeCalValue=0;
+            AfterCalValue=0;
             userValue.setText("");
+            output.setText("");
+            SplitString="";
             return;
         }
 
-        ////
+
         try {
             lastValue=userValue.getText().charAt(userValue.getText().length() - 1); //
         }catch (Exception e)
@@ -147,49 +191,123 @@ public class MainDesignController implements Initializable
         }
 
         System.out.println(lastValue);
-        if(lastValue=='+' || lastValue=='-' || lastValue=='x' || lastValue=='/')
+        if(lastValue=='+' || lastValue=='-' || lastValue=='x' || lastValue=='/' || lastValue=='.' || lastValue=='%')
         {
-            System.out.println(userValue.getText().length());
+            //System.out.println(userValue.getText().length());
+            //When Already add any Operator then user again push to change this .. then this Function will be work
             String RemoveValue=removeLastChar(userValue.getText());
             userValue.setText(RemoveValue);
 
-            if(actionEventForCB.getSource()==multi)
+            //System.out.println("Shit: "+lastValue);
+
+            if(StoreCalculationValue==0)
+            { BeforeCalValue= Double.parseDouble(SplitString); }
+            else
+            { BeforeCalValue=StoreCalculationValue; }
+
+            SplitString="";
+
+            if(actionEventForCB.getSource()==plus)
             {
-                PrintMethod(-1,ValueMulti);
-            }
-            else if(actionEventForCB.getSource()==plus)
-            {
+                fontSizeRestore();
+                conditionForOperator=1;
                 PrintMethod(-1,ValuePlus);
+                return;
             }
             else if(actionEventForCB.getSource()==subtraction)
             {
+                fontSizeRestore();
+                conditionForOperator=2;
                 PrintMethod(-1,ValueSubtraction);
+                return;
+            }
+            else if(actionEventForCB.getSource()==multi)
+            {
+                fontSizeRestore();
+                conditionForOperator=3;
+                PrintMethod(-1,ValueMulti);
+                return;
             }
             else if(actionEventForCB.getSource()==slash)
             {
-            PrintMethod(-1,ValueSlash);
+                fontSizeRestore();
+                conditionForOperator=4;
+                PrintMethod(-1,ValueSlash);
+                return;
             }
+            /////
+            else if(actionEventForCB.getSource()==full_stop)
+            {
+                fontSizeRestore();
+                PrintMethod(-1,ValueFull_Stop);
+                return;
+            }
+            else if(actionEventForCB.getSource()==percent)
+            {
+                fontSizeRestore();
+                conditionForOperator=5;
+                PrintMethod(-1,ValuePercent);
+                calculationMethod();
+                OperandStore(Integer.toString(ValueNine));
+                return;
+            }
+
         }
         else
         {
-            if(actionEventForCB.getSource()==multi)
+            if(StoreCalculationValue==0)
+            { BeforeCalValue= Double.parseDouble(SplitString); }
+            else
+            { BeforeCalValue=StoreCalculationValue; }
+
+
+            SplitString="";
+
+            if(actionEventForCB.getSource()==plus)
             {
-                PrintMethod(-1,ValueMulti);
-            }
-            else if(actionEventForCB.getSource()==plus)
-            {
+                fontSizeRestore();
+                conditionForOperator=1;
                 PrintMethod(-1,ValuePlus);
+                return;
             }
             else if(actionEventForCB.getSource()==subtraction)
             {
+                fontSizeRestore();
+                conditionForOperator=2;
                 PrintMethod(-1,ValueSubtraction);
+                return;
+            }
+            else if(actionEventForCB.getSource()==multi)
+            {
+                fontSizeRestore();
+                conditionForOperator=3;
+                PrintMethod(-1,ValueMulti);
+                return;
             }
             else if(actionEventForCB.getSource()==slash)
             {
+                fontSizeRestore();
+                conditionForOperator=4;
                 PrintMethod(-1,ValueSlash);
+                return;
+            }
+            ////
+            else if(actionEventForCB.getSource()==full_stop)
+            {
+                fontSizeRestore();
+                PrintMethod(-1,ValueFull_Stop);
+                return;
+            }
+            else if(actionEventForCB.getSource()==percent)
+            {
+                fontSizeRestore();
+                conditionForOperator=5;
+                PrintMethod(-1,ValuePercent);
+                OperandStore(Integer.toString(ValueNine)); //For Direct action in Output
+                calculationMethod();                      //For Direct action in Output
+                return;
             }
         }
-        ///////////
     }
 
     public void KeyPressAction(KeyEvent keyEvent)
@@ -197,60 +315,88 @@ public class MainDesignController implements Initializable
         //System.out.println(keyEvent.getCode().toString());
         if(keyEvent.getCode().toString().equals("DIGIT1") || keyEvent.getCode().toString().equals("NUMPAD1"))
         {
-            System.out.println("One");
             PrintMethod(ValueOne,"Emran");
+            OperandStore(Integer.toString(ValueOne));
+            return;
         }
         else if(keyEvent.getCode().toString().equals("DIGIT2") || keyEvent.getCode().toString().equals("NUMPAD2"))
         {
             PrintMethod(ValueTwo,"Emran");
+            OperandStore(Integer.toString(ValueTwo));
+            return;
         }
         else if(keyEvent.getCode().toString().equals("DIGIT3") || keyEvent.getCode().toString().equals("NUMPAD3"))
         {
             PrintMethod(ValueThree,"Emran");
+            OperandStore(Integer.toString(ValueThree));
+            return;
         }
         else if(keyEvent.getCode().toString().equals("DIGIT4") || keyEvent.getCode().toString().equals("NUMPAD4"))
         {
             PrintMethod(ValueFour,"Emran");
+            OperandStore(Integer.toString(ValueFour));
+            return;
         }
         else if(keyEvent.getCode().toString().equals("DIGIT5") || keyEvent.getCode().toString().equals("NUMPAD5"))
         {
             PrintMethod(ValueFive,"Emran");
+            OperandStore(Integer.toString(ValueFive));
+            return;
         }
         else if(keyEvent.getCode().toString().equals("DIGIT6") || keyEvent.getCode().toString().equals("NUMPAD6"))
         {
             PrintMethod(ValueSix,"Emran");
+            OperandStore(Integer.toString(ValueSix));
+            return;
         }
         else if(keyEvent.getCode().toString().equals("DIGIT7") || keyEvent.getCode().toString().equals("NUMPAD7"))
         {
             PrintMethod(ValueSeven,"Emran");
+            OperandStore(Integer.toString(ValueSeven));
+            return;
         }
         else if(keyEvent.getCode().toString().equals("DIGIT8") || keyEvent.getCode().toString().equals("NUMPAD8"))
         {
             PrintMethod(ValueEight,"Emran");
+            OperandStore(Integer.toString(ValueEight));
+            return;
         }
         else if(keyEvent.getCode().toString().equals("DIGIT9") || keyEvent.getCode().toString().equals("NUMPAD9"))
         {
             PrintMethod(ValueNine,"Emran");
+            OperandStore(Integer.toString(ValueNine));
+            return;
         }
         else if(keyEvent.getCode().toString().equals("DIGIT0") || keyEvent.getCode().toString().equals("NUMPAD0"))
         {
             PrintMethod(ValueZero,"Emran");
+            OperandStore(Integer.toString(ValueZero));
+            return;
         }
         else if(keyEvent.getCode().toString().equals(ValueZeroZero))
         {
             PrintMethod(-1,ValueZeroZero);
+            OperandStore(ValueZeroZero);
+            return;
         }
         else if(keyEvent.getCode().toString().equals("DECIMAL"))
         {
             PrintMethod(-1,ValueFull_Stop);
+            return;
         }
         else if(keyEvent.getCode().toString().equals("EQUALS"))
         {
-            PrintMethod(-1,ValueEqual);
+            calculationMethod();
+            userValue.setFont(new Font("Serif",13));
+            output.setFont(new Font("Serif", 25));
+            userValue.setText(userValue.getText());
+            output.setText(String.valueOf(StoreCalculationValue));
+            return;
         }
         else if(keyEvent.getCode().toString().equals(""))
         {
             PrintMethod(-1,ValuePercent);
+            return;
         }
         else if(keyEvent.getCode().toString().equals("BACK_SPACE"))
         {
@@ -267,7 +413,14 @@ public class MainDesignController implements Initializable
         }
         else if(keyEvent.getCode().toString().equals("A"))
         {
-            userValue.setText(ValueAC);
+            StoreCalculationValue=0;
+            BeforeCalValue=0;
+            AfterCalValue=0;
+            userValue.setFont(new Font("Serif",19));
+            output.setFont(new Font("Serif",19));
+            userValue.setText("");
+            output.setText("");
+            SplitString="";
             return;
         }
 
@@ -281,45 +434,77 @@ public class MainDesignController implements Initializable
 
         if(lastValue=='+' || lastValue=='-' || lastValue=='x' || lastValue=='/')
         {
+            System.out.println(userValue.getText().length());
             String RemoveValue=removeLastChar(userValue.getText());
             userValue.setText(RemoveValue);
 
+            if(StoreCalculationValue==0)
+            { BeforeCalValue= Double.parseDouble(SplitString); }
+            else
+            { BeforeCalValue=StoreCalculationValue; }
+
+            SplitString="";
+
             if(keyEvent.getCode().toString().equals("MULTIPLY"))
             {
+                conditionForOperator=3;
                 PrintMethod(-1,ValueMulti);
+                return;
             }
             else if(keyEvent.getCode().toString().equals("ADD"))
             {
+                conditionForOperator=1;
                 PrintMethod(-1,ValuePlus);
+                return;
             }
             else if(keyEvent.getCode().toString().equals("SUBTRACT"))
             {
+                conditionForOperator=2;
                 PrintMethod(-1,ValueSubtraction);
+                return;
             }
 
             else if(keyEvent.getCode().toString().equals("DIVIDE"))
             {
+                conditionForOperator=4;
                 PrintMethod(-1,ValueSlash);
+                return;
             }
         }
         else
         {
+            if(StoreCalculationValue==0)
+            { BeforeCalValue= Double.parseDouble(SplitString); }
+            else
+            { BeforeCalValue=StoreCalculationValue; }
+
+
+            SplitString="";
+
             if(keyEvent.getCode().toString().equals("MULTIPLY"))
             {
+                conditionForOperator=3;
                 PrintMethod(-1,ValueMulti);
+                return;
             }
             else if(keyEvent.getCode().toString().equals("ADD"))
             {
+                conditionForOperator=1;
                 PrintMethod(-1,ValuePlus);
+                return;
             }
             else if(keyEvent.getCode().toString().equals("SUBTRACT"))
             {
+                conditionForOperator=2;
                 PrintMethod(-1,ValueSubtraction);
+                return;
             }
 
             else if(keyEvent.getCode().toString().equals("DIVIDE"))
             {
+                conditionForOperator=4;
                 PrintMethod(-1,ValueSlash);
+                return;
             }
         }
 
@@ -332,6 +517,7 @@ public class MainDesignController implements Initializable
 
     public void PrintMethod(int valueI,String valueS)
     {
+        output.setText(String.valueOf(StoreCalculationValue));
         if(valueI!=-1)
         {
             String ValueConvertToS;
@@ -362,6 +548,60 @@ public class MainDesignController implements Initializable
 
         }
     }
+
+    public void OperandStore(String Value)
+    {
+        SplitString=SplitString+Value;
+        System.out.println("Connected:"+SplitString);
+        calculationMethod();
+        //calculationMethod(SplitString,PublicOperator);
+        //return;
+    }
+
+    public void calculationMethod()
+    {
+        //System.out.println("conditionForOperator: "+conditionForOperator);
+        double splitPartOne= Double.parseDouble(SplitString);
+
+        AfterCalValue=splitPartOne;
+
+        if(conditionForOperator==1)
+        {
+            StoreCalculationValue= BeforeCalValue+AfterCalValue;
+            System.out.println("1...StoreCalculationValue: "+StoreCalculationValue);
+        }
+        else if(conditionForOperator==2)
+        {
+            StoreCalculationValue= BeforeCalValue-AfterCalValue;
+            System.out.println("2...StoreCalculationValue: "+StoreCalculationValue);
+        }
+        else if(conditionForOperator==3)
+        {
+            StoreCalculationValue= BeforeCalValue*AfterCalValue;
+            System.out.println("3...StoreCalculationValue: "+StoreCalculationValue);
+        }
+        else if(conditionForOperator==4)
+        {
+            StoreCalculationValue= BeforeCalValue/AfterCalValue;
+            System.out.println("4...StoreCalculationValue: "+StoreCalculationValue);
+        }
+        else if(conditionForOperator==5)
+        {
+            StoreCalculationValue= BeforeCalValue/100;
+        }
+        output.setText(String.valueOf(StoreCalculationValue));
+
+    }
+
+    public void fontSizeRestore()
+    {
+        userValue.setFont(new Font("Serif",19));
+        output.setFont(new Font("Serif",19));
+    }
+
+    //RunTimeFrame
+
+
 
 
 }
